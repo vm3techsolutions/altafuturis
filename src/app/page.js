1cb851d9eaa9c5dcfdf4a6aa08244f6af163118d
Pooja Raut <poojaraut2618@gmail.com>
2025-02-14 18:27:13 +0530

    Add logo & team section on home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import AiSolutionCards from "./home/AiSolutionCards";
 import HomeBanner from "./home/HomeBanner";
 import WhoWeAre from "./home/WhoWeAre";
 import WhyChooseUs from "./home/WhyChooseUs";
+import ClientLogo from './home/ClientLogo';
+import OurTeam from './home/OurTeam';
 
 
 export default function Home() {
@@ -12,6 +14,8 @@ export default function Home() {
   <WhoWeAre/>
   <AiSolutionCards/>
   <WhyChooseUs/>
+  <ClientLogo/>
+  <OurTeam/>
 </div>
   );
 }
