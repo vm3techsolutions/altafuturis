9e9c7d7446007078b26c1c9ee89ac85f189ed7c7
poojaraut2618 <poojaraut2618@gmail.com>
2025-03-20 18:15:28 +0530

    update in page title

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true, // Disables Next.js automatic image optimization
+  },
   output: 'export'
 };
 
--- src/app/services/PageTitle.jsx
+++ src/app/services/PageTitle.jsx
@@ -1,4 +1,4 @@
-
+"use client";
 
 function PageTitle() {
   return (
