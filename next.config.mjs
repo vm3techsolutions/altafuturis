20e9c42c5db98173a9b35733746dd718b4147c81
Vishal Bhange <vishalbhange333@gmail.com>
2025-03-20 11:45:37 +0530

    img issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      unoptimized: true, // Disable Next.js image optimization
+    },
+  };
+  
+  export default nextConfig;
+  
