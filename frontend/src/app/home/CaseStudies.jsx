0db2a10c3ee613f94bb4fdd2d3d964a134c70211
RutikGaikwad123 <ruttikgaikwad@gmail.com>
2025-03-31 17:05:27 +0530

    home casestudy env add

--- frontend/src/app/home/CaseStudies.jsx
+++ frontend/src/app/home/CaseStudies.jsx
@@ -12,7 +12,8 @@ const PortfolioSection = () => {
   useEffect(() => {
     const fetchCaseStudies = async () => {
       try {
-        const response = await axios.get("http://localhost/resources/wp-json/wp/v2/case_study?_embed");
+        // const response = await axios.get("http://localhost/resources/wp-json/wp/v2/case_study?_embed");
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_WORDPRESS_API}/case_study?_embed`);
         const data = response.data.slice(0, 3).map((study) => ({
           slug: study.slug, // Use slug instead of id
           title: study.title.rendered,
