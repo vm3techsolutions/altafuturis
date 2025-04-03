74f85f765ce6f998e68e40bc1ea46ff75b0226fc
poojaraut2618 <poojaraut2618@gmail.com>
2025-04-03 14:59:47 +0530

    update img tag in case study, blog file

--- frontend/src/app/blog/[slug]/page.js
+++ frontend/src/app/blog/[slug]/page.js
@@ -135,7 +135,7 @@ const BlogPostDetails = async ({ params }) => {
                   <div key={post.slug} className="bg-white p-4 rounded-lg flex flex-col md:flex-row">
                     {/* ✅ Image */}
                     <div className="w-full md:w-1/3">
-                      <Image
+                      <img
                         className="w-full h-40 object-cover border-brownColor border-2 rounded-md"
                         src={post.image}
                         width={300}
