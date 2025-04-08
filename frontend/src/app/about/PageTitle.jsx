ab773a3a223cf83ad50d458828f5bd7cb6182405
poojaraut2618 <poojaraut2618@gmail.com>
2025-04-08 18:11:08 +0530

    update about component to increase performance
    update about component to increase performance

--- frontend/src/app/about/PageTitle.jsx
+++ frontend/src/app/about/PageTitle.jsx
@@ -1,12 +1,16 @@
 "use client";
 
+import Image from "next/image";
+
 function PageTitle() {
   return (
     <>
       <div className="relative w-full md:mt-32 lg:mt-32 h-[30vh] md:h-[75vh] lg:h-[65vh] bg-cover bg-center flex items-center overflow-hidden">
-        <img
+        <Image
           src="/assets/aboutBanner2.png"
           alt="About Page Title"
+          width={1400}
+          height={500}
           className="w-full h-full object-auto object-center"
           loading="lazy"
         />
